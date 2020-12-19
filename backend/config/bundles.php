f8cb1703235a09f38b81d387912a1a5a9b065832
Alexander Reifinger <alexander.reifinger@check24.de>
2020-12-19 11:59:33 +0100

    Initial commit

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
